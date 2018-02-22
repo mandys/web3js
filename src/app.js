dca2fc2ebece4d6ace770096bb0cd784c7540841
Mandy Singh <mandys@gmail.com>
2018-02-22 16:40:12 +0530

    updating with todos

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ getWeb3.then((results) => {
     web3 = results.web3;
     console.log("=====LOGGING web3======");
     console.log(web3);
+    // check if metamask is not installed, throw alert and exit
+    // find which network is user connected to ?
+    // list all user accounts from metamask
+    // pull balance of the eth0 (coinbase) account
 }).catch((e) => {
     console.log(e);
     web3 = null;
